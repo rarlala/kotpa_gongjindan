c700a42f6e1f219de8ebec612912e85ba3b641c2
JEONGSUJI <wtw9611@gmail.com>
2020-06-08 23:30:15 +0900

    bugfix: scroll 문제 해결

--- src/js/common.js
+++ src/js/common.js
@@ -1,17 +1,16 @@
-$(function() {
-
+$(function () {
   // header
   // header follow, sns 내부 ul 내 li 아이콘 색상 hover 시 변경 처리
 
   $('header .group .follow-list li img')
-    .on('mouseover', function() {
+    .on('mouseover', function () {
       var nowLi = $(this).attr('src');
       var hoverLi = nowLi.split('_off')[0];
       $(this)
         .stop()
         .attr('src', hoverLi + '_on.png');
     })
-    .on('mouseleave', function() {
+    .on('mouseleave', function () {
       var now2Li = $(this).attr('src');
       var hover2Li = now2Li.split('_on')[0];
       $(this)
@@ -20,14 +19,14 @@ $(function() {
     });
 
   $('header .group .sns-list li img')
-    .on('mouseover', function() {
+    .on('mouseover', function () {
       var nowLi = $(this).attr('src');
       var hoverLi = nowLi.split('_off')[0];
       $(this)
         .stop()
         .attr('src', hoverLi + '_on.png');
     })
-    .on('mouseleave', function() {
+    .on('mouseleave', function () {
       var now2Li = $(this).attr('src');
       var hover2Li = now2Li.split('_on')[0];
       $(this)
@@ -38,7 +37,7 @@ $(function() {
   // header follow, sns 내부 ul slide 처리
 
   $('.btn-follow')
-    .on('mouseover', function() {
+    .on('mouseover', function () {
       if ($(window).width() > 783) {
         $('.follow-list').slideDown();
       } else {
@@ -47,7 +46,7 @@ $(function() {
         });
       }
     })
-    .on('mouseleave', function() {
+    .on('mouseleave', function () {
       if ($(window).width() > 783) {
         $('.follow-list').slideUp();
       } else {
@@ -58,7 +57,7 @@ $(function() {
     });
 
   $('.btn-sns')
-    .on('mouseover', function() {
+    .on('mouseover', function () {
       if ($(window).width() > 783) {
         $('.sns-list').slideDown();
       } else {
@@ -67,7 +66,7 @@ $(function() {
         });
       }
     })
-    .on('mouseleave', function() {
+    .on('mouseleave', function () {
       if ($(window).width() > 783) {
         $('.sns-list').slideUp();
       } else {
@@ -78,9 +77,13 @@ $(function() {
     });
 
   // menu 버튼 클릭 시 메뉴 나오게 처리
-  $('.btn-menu').on('click', function() {
+  $('.btn-menu').on('click', function () {
     $('.dark-bg').stop().css({
       display: 'inline-block',
+    });
+
+    $('.header-box .logo').css({
+      'z-index': '10',
     });
 
     // 메인 페이지와 세부 페이지에 따른 nav 위치가 달라 조건 분기
@@ -99,9 +102,13 @@ $(function() {
 
   // menu 닫기 버튼 클릭 시 메뉴 닫히도록 처리
 
-  $('.btn-close-menu').on('click', function() {
+  $('.btn-close-menu').on('click', function () {
     $('.dark-bg').stop().css({
       display: 'none',
+    });
+
+    $('.header-box .logo').css({
+      'z-index': '20',
     });
 
     $('nav').stop().animate({
@@ -112,9 +119,12 @@ $(function() {
   });
 
   // dark-bg 클릭 시 메뉴 닫히도록 처리
-  $('.dark-bg').on('click', function() {
+  $('.dark-bg').on('click', function () {
     $('.dark-bg').stop().css({
       display: 'none',
+    });
+    $('.header-box .logo').css({
+      'z-index': '20',
     });
     $('nav').stop().animate({
       right: '-120%',
@@ -125,7 +135,7 @@ $(function() {
   // banner
 
   // banner event 2개 3초마다 번갈아가며 출력
-  setInterval(function() {
+  setInterval(function () {
     $('.banner li').fadeToggle('hidden');
   }, 3000);
 
@@ -134,12 +144,12 @@ $(function() {
   var $window = $(window),
     $btnTop = $('.top');
 
-  $btnTop.on('click', function() {
-    $('html').animate({
+  $btnTop.on('click', function () {
+    $('html').animate(
+      {
         scrollTop: 0,
       },
       500
     );
   });
-
-})
+});
